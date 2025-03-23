5c76260facaba81006a0a7910497c17e3c543e22
Boris Anthony <964649+BorisAnthony@users.noreply.github.com>
2025-03-23 21:34:14 +0900

    remove temporary comments

--- src/MeasurementUnitInterface.php
+++ src/MeasurementUnitInterface.php
@@ -7,13 +7,13 @@ use Stringable;
 
 interface MeasurementUnitInterface extends Stringable
 {
-    public function getValue(): float; // Untested
+    public function getValue(): float;
 
-    public function getInstanceSymbol(): string; // Untested
+    public function getInstanceSymbol(): string;
 
-    public function setInstanceSymbol(string $symbol): string; // Untested
+    public function setInstanceSymbol(string $symbol): string;
 
     public static function getSymbol(): string;
 
-    public static function setSymbol(string $symbol): string; // Untested
+    public static function setSymbol(string $symbol): string;
 }
